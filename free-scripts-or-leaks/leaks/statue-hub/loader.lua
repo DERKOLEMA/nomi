26806fc95d6570f61e92f982aad2dcffa2e60403
Duder <84186185+Duderpast@users.noreply.github.com>
2023-02-02 00:05:09 +0200

    Update loader.lua

--- free-scripts-or-leaks/leaks/statue-hub/loader.lua
+++ free-scripts-or-leaks/leaks/statue-hub/loader.lua
@@ -1,1 +1,16 @@
+local request = (request or http_request or syn and syn.request)
 
+local get_game = loadstring(game:HttpGet("https://raw.githubusercontent.com/Duderpast/nomi/main/free-scripts-or-leaks/leaks/statue-hub/scripts/Games"))()
+
+local games_list = request({
+	Url = "https://google.com",
+	Method = "GET"
+})
+
+games_list = (games_list.Body)
+for get_game, games_list in next, get_game do
+	if games_list == game.PlaceId then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Duderpast/nomi/main/free-scripts-or-leaks/leaks/statue-hub/scripts/" .. get_game))()
+		return
+	end
+end
